9a17ddf0a910254b366997fe162df76a38d90659
Ayo-lenz <johnlenzvictor@gmail.com>
2025-03-20 18:09:26 +0100

    add search text functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -21,25 +21,56 @@ const PromptCardList = ({data, handleTagClick}) => {
 }
 
 const Feed = () => {
-
-  const [searchText, setSearchText] = useState('');
   const [posts, setPosts] = useState([])
 
-  const handleSearchChange = (e) => {
+   // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+  
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
 
-  }
+    setPosts(data);
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch('/api/prompt') //Again we have to create this api endpoint
-      const data = await res.json();
-
-      setPosts(data)
-    }
-
-    fetchPosts()
+    fetchPosts();
   }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+      regex.test(item.creator.username) ||
+      regex.test(item.tag) ||
+      regex.test(item.prompt)
+    );
+  };
   
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
+
   return (
     <section className="feed">
       <form action="" className='relative w-full text-center'>
@@ -47,16 +78,22 @@ const Feed = () => {
           type="text"
           placeholder= 'Search for a tag or a username'
           value={searchText}
-          onchange={handleSearchChange}
+          onChange={handleSearchChange}
           required
           className='search_input peer'
         />
       </form>
 
-      <PromptCardList
-        data={posts}
-        handleTagClick={() => {}} 
-      />
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
+
     </section>
   )
 }
